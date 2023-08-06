61775bcac7417181b9935231a6348aa394fd5c16
rodrigo-garcez <rodrigo.hermann@bandtec.com.br>
2023-08-06 20:39:44 -0300

    Arrumando o print da lambda

--- IaC/raw_to_staged/app.py
+++ IaC/raw_to_staged/app.py
@@ -1,6 +1,7 @@
 import json
 
 def lambda_handler(event, context):
+    print(event)
     for record in event['Records']:
         bucket_name = record['s3']['bucket']['name']
         object_key = record['s3']['object']['key']
