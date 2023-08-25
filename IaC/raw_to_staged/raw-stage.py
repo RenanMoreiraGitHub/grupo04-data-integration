7f4e2113e972d04b49904002c12563374c543cc8
DantasEduardo <79462590+DantasEduardo@users.noreply.github.com>
2023-08-24 22:03:14 -0300

    update lambda raw-stage.py

--- IaC/raw_to_staged/raw-stage.py
+++ IaC/raw_to_staged/raw-stage.py
@@ -1,14 +1,9 @@
 import boto3
-from pprint import pp
-import re
+import json
 import src.utils.utils as utils
-import pyspark.sql.functions as sf
-from datetime import datetime
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, DateType
-from pyspark.sql.functions import from_json
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
 
-boto3.setup_default_session(profile_name='faculdade')
 
 BUCKET = 'raw-soybean-gp4-sptech'
 
@@ -23,7 +18,7 @@ def main():
         
     s3 = boto3.resource('s3')
     try:
-        s3.meta.client.download_file(BUCKET, last_filie,last_filie)
+       s3.meta.client.download_file(BUCKET, last_filie,last_filie)
     except Exception as e:
         print(e)
         raise "ERROR GETTING OBJECT."
@@ -31,12 +26,11 @@ def main():
     with open(f'./{last_filie}', "r") as f:
         file_content = f.read()
     
-    import json
+
+    
     file_content = json.loads(file_content)
 
-    print((file_content))
-
-    schema = StructType([StructField("device_id", StringType(), nullable=False),
+    schema = StructType([StructField("device_id", StringType(), nullable=True),
                          StructField("device_name", StringType(), nullable=True),
                          StructField("temperature", DoubleType(), nullable=True),
                          StructField("pressure", DoubleType(), nullable=True),
@@ -48,16 +42,16 @@ def main():
                          StructField("capacity", DoubleType(), nullable=True),
                          StructField("collected", DoubleType(), nullable=True),
                          StructField("humidity_grain", DoubleType(), nullable=True),
-                         StructField("batery", DoubleType(), nullable=True)])
+                         StructField("batery", DoubleType(), nullable=True),
+                         StructField("setor", StringType(), nullable=True)])
     
     data = [(file_content['device_id'], file_content['device_name'], 
              file_content['temperature'], file_content['pressure'], 
              file_content['air-speed'], file_content['n'], file_content['p'], 
              file_content['k'], file_content['humidity'], file_content['capacity'], 
              file_content['collected'], file_content['humidity_grain'],
-             file_content['batery'])]
+             file_content['batery'], file_content['setor'])]
 
-    # Crie um RDD a partir da lista de tuplas
     rdd = spark.sparkContext.parallelize(data)
 
     df = spark.createDataFrame(rdd, schema=schema)    
@@ -65,7 +59,7 @@ def main():
        
     last_filie = last_filie.replace(".json", "")
     df.write.csv(f"s3a://stagged-soybean-gp4-sptech/{last_filie}.csv", header=True, mode="overwrite")
- 
+    
 if __name__ == "__main__":
     main()
     
