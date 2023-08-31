948beac5af7463001d2fb3c47cefd43ffffed87d
EnanHenrique <enan.oliveira@bandtec.com.br>
2023-08-30 21:59:40 -0300

    add column float dataset prices

--- external-data/insert_prices.py
+++ external-data/insert_prices.py
@@ -11,6 +11,11 @@ df_prices = pd.read_csv('./data/Soybean-Price.csv', sep=';')
 df_prices['region'] = 'brazil'
 df_prices['date'] = pd.to_datetime(df_prices['date'])
 
+df_prices["real"] = df_prices["real"].str.replace(',','.')
+df_prices["usd"] = df_prices["usd"].str.replace(',','.')
+df_prices['real'] = df_prices['real'].astype('float64')
+df_prices['usd'] = df_prices['usd'].astype('float64')
+
 downloads_path = str(join(Path.home(), "Downloads"))
 print(f'Downloading dataset prices to {downloads_path}...')
 df_prices.to_excel(join(downloads_path, 'dataset_prices_soybean.xlsx'), index=True)
