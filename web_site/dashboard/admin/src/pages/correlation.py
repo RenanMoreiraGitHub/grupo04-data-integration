a29b18026985c730fc0da087a132f254fcd37b53
Eduardo Dantas de Oliveira <eduardo.oliveira@yhub.com.br>
2023-10-29 15:56:50 -0300

    add new dashboard analytics

--- web_site/dashboard/admin/src/pages/correlation.py
+++ web_site/dashboard/admin/src/pages/correlation.py
@@ -1,6 +1,32 @@
+import cv2
 import streamlit as st
 import plotly.express as px
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
+def slice(path_img:str, path_save:str) -> None:
+    img = plt.imread(path_img)[58:-55, 192:-98]
+    plt.imsave(path_save, img)
+
+
+def calc_percentage(img_path:str) -> float:
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    st.image(gray)
+
+    ret, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+    st.image(thresh)
+    
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 3)
+    st.image(opening)
+
+    sure_bg = cv2.dilate(opening,kernel,iterations = 4)
+    st.image(sure_bg)
+
+    percentage = (np.count_nonzero(sure_bg == 0) * 100)/(sure_bg.shape[0] * sure_bg.shape[1])
+    return round(percentage, 2)
 
 
 def render(df_export, df_rain):
@@ -8,6 +34,7 @@ def render(df_export, df_rain):
     with st.container():
         temporal_slct = st.radio("Visão por", ('Dia', 'Mes', 'Ano'), horizontal=True)
         group_slct = st.radio("Agrupado por", ('Estados', 'Nenhum'), horizontal=True)
+        scope = st.radio("Escopo", ('trace', 'overall'), horizontal=True)
         
         if group_slct == 'Estados':
             options_slct = st.radio("Selecionar por", ('Top 10 maiores exportadores', 'Selecionar manualmente'), horizontal=True)
@@ -34,7 +61,9 @@ def render(df_export, df_rain):
                 df_merged,
                 x='precipitation', 
                 y='kg',
-                color='state'
+                color='state',
+                trendline="ols",
+                trendline_scope=scope
             )
         
         else:
@@ -48,6 +77,7 @@ def render(df_export, df_rain):
                 df_merged,
                 x='precipitation', 
                 y='kg',
+                trendline="ols"
             )
 
         graf.update_layout(
@@ -58,3 +88,19 @@ def render(df_export, df_rain):
 
         st.plotly_chart(graf, use_container_width=True)
     
+    st.header("Exemplo do processo de Cálculo de porcentagem de núvens na região")
+    with st.container():
+        uploaded_file = st.file_uploader("Upload Imagem", type=['png', 'jpg','jpeg'])
+        if uploaded_file is not None:
+            img = Image.open(uploaded_file)
+            img_array = np.array(img)
+            
+            plt.imsave(r'datasets\data_raw\img.png', img_array)
+            st.image(img)
+            
+            slice(r'datasets\data_raw\img.png', 
+                  r'datasets\data\img.png')
+
+            percentage = calc_percentage(r'datasets\data\img.png')
+            st.text(f"Porcentagem: {percentage - 0.7}%")
+            
